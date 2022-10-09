cecbbfe3e1740ee18e7d5dca8ee4d4d0cc6f6458
Mateus Lopes de Castro <mateuslopescastro@outlook.com>
2022-10-09 01:22:11 -0300

    feat: generating home page statically

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import css from "../styles/pages/home.module.scss";
+import { formatMonetaryValue } from "../utils/formatMonetaryValue";
 
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   };
 }
 
@@ -41,20 +42,18 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1LqqPnGrbHDJN7JnxjdXlxDN");
 
   const product = {
     priceId: price.id,
-    amount: new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }).format(price.unit_amount / 100),
+    amount: formatMonetaryValue(price.unit_amount / 100),
   };
 
   return {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24 * 15, // 15 days
   };
 };
