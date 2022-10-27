e7e5534447233971720f61b0f355995395a388cc
Mateus Lopes de Castro <mateuslopescastro@outlook.com>
2022-10-27 20:48:02 -0300

    feat: posts screen

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,53 @@
+import Head from "next/head";
+
+import css from "../../styles/pages/posts.module.scss";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | ig news</title>
+      </Head>
+
+      <main className={css.container}>
+        <div className={css.posts}>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
