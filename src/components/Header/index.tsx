7216f114c00aa133b96c0bafbbc974f8dd08eab8
Mateus Lopes de Castro <mateuslopescastro@outlook.com>
2022-10-27 22:36:32 -0300

    feat: navigate between items + active item style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,31 @@
-import css from "./styles.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { SignInButton } from "../SignInButton";
 
+import css from "./styles.module.scss";
+
+const NAV_ITEMS = [
+  { href: "/", title: "Home" },
+  { href: "/posts", title: "Posts" },
+];
+
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={css.headerContainer}>
       <div className={css.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a href="" className={css.active}>
-            Home
-          </a>
-          <a href="">Posts</a>
+          {NAV_ITEMS.map((item) => (
+            <Link href={item.href} key={item.href}>
+              <a className={asPath === item.href ? css.active : ""}>
+                {item.title}
+              </a>
+            </Link>
+          ))}
         </nav>
 
         <SignInButton />
